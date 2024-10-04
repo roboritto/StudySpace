1ec801ed48b2559ede88ae897571223197f0184f
adlinaliya <nur_24003411@utp.edu.my>
2024-10-05 02:07:00 +0800

    Add files via upload

---
+++ Car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudySpace
+{
+    internal class Car
+    {
+        public float Speed { get; set; }
+        public BitVector32 Position { get; set; }
+        public BitVector32 Direction { get; set; }
+
+        public Car(float speed, BitVector32 position, BitVector32 direction) 
+        {
+            Speed = speed;
+            Position = position;
+            Direction = direction;
+        }
+
+        public void Move()
+        {
+            return;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine(Convert.ToString(Speed));
+        }
+    }
+}
---
+++ Driver.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudySpace
+{
+    internal class Driver
+    {
+        public string Name { get; set; }
+        public string Skill {  get; set; }
+
+        public Driver(string name, string skill) 
+        { 
+            Name = name; 
+            Skill = skill; 
+        }
+    }
+}
